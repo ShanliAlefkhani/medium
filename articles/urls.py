a3be3796cc7209ebe8cd1bd81ae31398a125a656
Shanli <shanli.alefkhani@tapsi.net>
2024-05-14 22:19:15 +0330

    add RateCreate view

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,6 @@ from articles import views
 
 urlpatterns = [
 	path('', views.ArticleList.as_view()),
+	path('rates', views.RateCreate.as_view()),
 ]
 
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,22 @@
-from rest_framework import generics
-from articles.models import Article
-from articles.serializers import ArticleSerializer
+from rest_framework import generics, status
+from articles.models import Article, Rate
+from articles.serializers import ArticleSerializer, RateSerializer
+from rest_framework.response import Response
 
 
 class ArticleList(generics.ListAPIView):
 	queryset = Article.objects.all()
 	serializer_class = ArticleSerializer
+
+class RateCreate(generics.CreateAPIView):
+	queryset = Rate.objects.all()
+	serializer_class = RateSerializer
+
+	def create(self, request, *args, **kwargs):
+		rate, created = Rate.objects.update_or_create(
+			user=request.user,
+			defaults={'star': request.data.get('star')}
+		)
+		serializer = self.get_serializer(rate)
+		return Response(serializer.data, status=status.HTTP_201_CREATED if created else status.HTTP_200_OK)
 
