c1a0f15f6829decfae41baaf66c5e136d39f458d
Shanli <shanli.alefkhani@tapsi.net>
2024-05-15 22:09:55 +0330

    add user rate to ArticleSerializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -11,6 +11,18 @@ class ArticleSerializer(serializers.ModelSerializer):
 	ratings = RateSerializer(source='rate_set', many=True)
 	number_of_users_rated = serializers.ReadOnlyField()
 	ratings_average = serializers.ReadOnlyField()
+	user_rate = serializers.SerializerMethodField()
+
+	def get_user_rate(self, obj):
+		request = self.context.get('request')
+		if request and request.user.is_authenticated:
+			user = request.user
+			try:
+				rate = Rate.objects.get(article=obj, user=user)
+				return rate.star
+			except Rate.DoesNotExist:
+				return None
+		return None
 
 	class Meta:
 		model = Article
