20a6b52c2f2de6db022bf578fa7a74482b6db8fe
Shanli <shanli.alefkhani@tapsi.net>
2024-05-15 22:12:38 +0330

    inherit from TimestampedModel

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.db.models import Avg
 from django.contrib.auth.models import User
+from timestampedmodel import TimestampedModel
 
-class Article(models.Model):
+
+class Article(TimestampedModel):
 	title = models.CharField(max_length=100)
 	ratings = models.ManyToManyField(User, through='Rate')
 
@@ -15,7 +17,7 @@ class Article(models.Model):
 		print(self.ratings.aggregate(Avg('rate__star')).get('rate__star__avg'))
 		return self.ratings.aggregate(Avg('rate__star')).get('rate__star__avg')
 
-class Rate(models.Model):
+class Rate(TimestampedModel):
 	article = models.ForeignKey(Article, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	star = models.IntegerField()
