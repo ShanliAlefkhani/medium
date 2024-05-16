15a6e6a85b7cf4c714c0f6982d7b854394950b2e
Shanli <shanli.alefkhani@tapsi.net>
2024-05-16 21:14:19 +0330

    add fraud detection logic

--- articles/tasks.py
+++ articles/tasks.py
@@ -1,17 +1,17 @@
 from celery import shared_task
 from celery.schedules import crontab
-from articles.utils import fill_rate_ratios
+from articles.utils import find_influenced_ratings
 from medium.celery import app as celery_app
 
 
 @shared_task
-def fill_rate_ratios_task():
-	fill_rate_ratios()
+def find_influenced_ratings_task():
+	find_influenced_ratings()
 
 celery_app.conf.beat_schedule = {
-    'run-every-20-minutes': {
+    'run-every-30-minutes': {
         'task': 'articles.tasks.fill_rate_ratios_task',
-        'schedule': crontab(minute='*/20'), 
+        'schedule': crontab(minute='*/30'),
     },
 }
 
--- articles/utils.py
+++ articles/utils.py
@@ -1,2 +1,27 @@
-def fill_rate_ratios():
-	print("filling")
+from articles.models import Rate
+from datetime import timedelta
+from django.utils import timezone
+
+
+def find_influenced_ratings(article_id):
+	time_threshold = 30
+	start_time = timezone.now() - timedelta(seconds=time_threshold)
+	star_average = Rate.objects.aggregate(weighted_avg=Avg(F('rate__star') * F('rate__ratio')))['weighted_avg'] / 100
+	recent_ratings = Rate.objects.filter(article_id=article_id, timestamp__gte=time_limit)
+
+	# TODO: Make these variables dynamic
+	x = 5
+	y = 0
+
+	if influenced_ratings := recent_ratings.filter(star__gt=star_average + y).count() >= x:
+		change_influenced_ratings_ratio(influenced_ratings)
+
+	if influenced_ratings := recent_ratings.filter(star__lt=star_average - y).count() >= x:
+		change_influenced_ratings_ratio(influenced_ratings)
+
+def change_influenced_ratings_ratio(ratings):
+	z = ratings.count()
+	for rating in recent_ratings:
+		rating.score /= z
+		rating.save()
+
